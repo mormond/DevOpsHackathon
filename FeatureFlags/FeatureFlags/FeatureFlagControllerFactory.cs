f01856e84920d9e8f5786cf9ee7c446705f2cd6f
Mike Ormond <mormond@hotmail.com>
2015-10-08 18:49:31 +0100

    Initial commit

---
+++ FeatureFlags/FeatureFlags/FeatureFlagControllerFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace FeatureFlags
+{
+    public class FeatureFlagControllerFactory : DefaultControllerFactory
+    {
+        IDirector director;
+        public FeatureFlagControllerFactory()
+        {
+            this.director = Director.GetInstance();
+        }
+
+        public override IController CreateController(RequestContext requestContext, string controllerName)
+        {
+            if (director.IsAnyFeatureEnabled() && !controllerName.EndsWith("_B"))
+            {
+                controllerName = $"{controllerName}_B";
+            }
+            return base.CreateController(requestContext, controllerName);
+
+        }
+
+        protected override Type GetControllerType(RequestContext requestContext, string controllerName)
+        {
+            if (director.IsAnyFeatureEnabled() && !controllerName.EndsWith("_B"))
+            {
+                controllerName = $"{controllerName}_B";
+            }
+            return base.GetControllerType(requestContext, controllerName);
+        }
+    }
+}
---
+++ FeatureFlags/FeatureFlags/IDirector.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+
+namespace FeatureFlags
+{
+    public interface IDirector
+    {
+        bool IsEnabled(string name, ExpandoObject args = null);
+
+        bool IsAnyFeatureEnabled(ExpandoObject args = null);
+
+        bool AreAllFeaturesEnabled(ExpandoObject args = null);
+    }
+}
