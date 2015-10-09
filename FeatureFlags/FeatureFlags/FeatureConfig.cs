a87728c18c5d6b3556f4b2412cf018a993c08eab
Mike Ormond <mormond@hotmail.com>
2015-10-09 10:06:02 +0100

    Added FeatureConfig.cs

---
+++ FeatureFlags/FeatureFlags/FeatureConfig.cs
@@ -0,0 +1,29 @@
+﻿using FeatureFlag;
+using FeatureFlag.Strategies;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FeatureFlags
+{
+    public class FeatureConfig
+    {
+        private Configuration config;
+
+        public FeatureConfig()
+        {
+            this.config = new Configuration();
+
+            Feature feature = new Feature("Fred", new OnStrategy(), null, DateTime.Parse("25/9/2015")); 
+
+            this.config.AddFeature(feature);
+        }
+
+        public IDirector CreateFeatureDirector()
+        {
+            return new Director(config);   
+        }
+
+    }
+}
